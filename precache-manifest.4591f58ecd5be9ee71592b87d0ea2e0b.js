728d4e3f119c35f14e2b9f2b87ddf23f25bae4b4
sharmayash10 <0yashs12@gmail.com>
2022-10-04 19:04:16 +0530

    Updates

---
+++ precache-manifest.4591f58ecd5be9ee71592b87d0ea2e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c5f60bbec3ebe014f8c",
+    "url": "/static/css/main.460ec37f.chunk.css"
+  },
+  {
+    "revision": "4c5f60bbec3ebe014f8c",
+    "url": "/static/js/main.4c5f60bb.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "ae45a617333b9dada5b6",
+    "url": "/static/js/2.ae45a617.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "abfbba2cdffe213f1c0ba6ada40d52f6",
+    "url": "/index.html"
+  }
+];
